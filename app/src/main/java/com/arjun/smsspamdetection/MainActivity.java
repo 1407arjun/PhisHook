bc3bc2e94f321d2efc5b0601c433f923883c719f
Arjun <arjun140702@gmail.com>
2022-01-19 11:40:47 +0530

    Add permissions

--- app/src/main/java/com/arjun/smsspamdetection/MainActivity.java
+++ app/src/main/java/com/arjun/smsspamdetection/MainActivity.java
@@ -1,14 +1,58 @@
 package com.arjun.smsspamdetection;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int READ_SMS_REQUEST_CODE = 100;
+    private ArrayList<String> messages = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_SMS}, READ_SMS_REQUEST_CODE);
+        else
+            getMessages();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) == PackageManager.PERMISSION_GRANTED) {
+                getMessages();
+            }
+        }
+    }
+
+    private void getMessages() {
+        Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                StringBuilder msgData = new StringBuilder();
+                for(int idx=0; idx < cursor.getColumnCount(); idx++) {
+                    msgData.append(" ").append(cursor.getColumnName(idx)).append(":").append(cursor.getString(idx));
+                }
+                messages.add(msgData.toString());
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
     }
 }
