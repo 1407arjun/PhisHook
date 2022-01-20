5fe7595c11174b4bbb6dff7aee114e0ad93a62c7
Arjun <arjun140702@gmail.com>
2022-01-20 12:13:59 +0530

    Add unreport button

--- app/src/main/java/com/arjun/smsspamdetection/MessageAdapter.java
+++ app/src/main/java/com/arjun/smsspamdetection/MessageAdapter.java
@@ -1,6 +1,7 @@
 package com.arjun.smsspamdetection;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import com.google.android.material.card.MaterialCardView;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -35,7 +38,7 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.Recycler
 
         private final TextView addressText, dateText, bodyText;
         private final MaterialCardView cardView;
-        private final Button reportButton;
+        private final Button reportButton, unreportButton;
 
         public RecyclerViewHolder(@NonNull View view) {
             super(view);
@@ -45,6 +48,7 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.Recycler
             bodyText = view.findViewById(R.id.bodyText);
             cardView = view.findViewById(R.id.cardView);
             reportButton = view.findViewById(R.id.reportButton);
+            unreportButton = view.findViewById(R.id.unreportButton);
         }
     }
 
@@ -58,15 +62,33 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.Recycler
         if (message.getResult() != null) {
             if (message.getResult().equals("Safe")) {
                 holder.cardView.setStrokeColor(context.getColor(android.R.color.holo_green_light));
-                holder.reportButton.setVisibility(View.GONE);
+                holder.unreportButton.setVisibility(View.GONE);
             } else {
                 holder.cardView.setStrokeColor(context.getColor(android.R.color.holo_red_light));
-                holder.reportButton.setVisibility(View.VISIBLE);
+                holder.unreportButton.setVisibility(View.VISIBLE);
             }
         } else {
             holder.cardView.setStrokeColor(context.getColor(android.R.color.transparent));
-            holder.reportButton.setVisibility(View.VISIBLE);
+            holder.unreportButton.setVisibility(View.VISIBLE);
        }
+
+        holder.reportButton.setOnClickListener(v -> {
+            new AlertDialog.Builder(context)
+                    .setIcon(R.drawable.ic_report)
+                    .setTitle("Success")
+                    .setMessage("Successfully reported to Cyber Security Department (CERT-In)")
+                    .setPositiveButton("Okay", null)
+                    .show();
+        });
+
+        holder.unreportButton.setOnClickListener(v -> {
+            new AlertDialog.Builder(context)
+                    .setIcon(R.drawable.ic_unreport)
+                    .setTitle("Success")
+                    .setMessage("Message marked as not spam or phishing")
+                    .setPositiveButton("Okay", null)
+                    .show();
+        });
     }
 
     @Override
